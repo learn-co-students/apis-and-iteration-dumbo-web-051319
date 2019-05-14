dc93858ab81c2d69e93112595bb4d2bddddb7ac6
tommaclean <tom.j.maclean@gmail.com>
2019-05-14 14:40:43 -0400

    done

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,19 @@
+require_relative "api_communicator"
+require 'pry'
+
 def welcome
   # puts out a welcome message here!
+  puts "Hello! Welcome!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
+  return input.downcase
 end
+
+
+welcome
+# # get_character_from_user
+puts show_character_movies(get_character_from_user)
