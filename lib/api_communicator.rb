b5ca6ac78fe60b3d2ece3ef2cdfe6c4ea4de932d
April <fanmeng0417@gmail.com>
2019-05-14 15:24:19 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -25,6 +25,29 @@ def get_character_movies_from_api(character_name)
   return ary
 end
 
+def get_movie_director_from_api(movie_name)
+  #make the web request
+  response_string = RestClient.get('http://www.swapi.co/api/films/')
+  response_hash = JSON.parse(response_string)
+  str = ""
+  # iterate over the response hash to find the collection of `films` for the given
+  #   `character`
+  # collect those film API urls, make a web request to each URL to get the info
+  #  for that film
+  filmes = response_hash["results"]
+  # return value of this method should be collection of info about each film.
+  filmes.each do |ele|
+    if ele["title"] == movie_name
+      str = ele["director"]
+    end
+  end
+  #  i.e. an array of hashes in which each hash reps a given film
+  # this collection will be the argument given to `print_movies`
+  #  and that method will do some nice presentation stuff like puts out a list
+  #  of movies by title. Have a play around with the puts with other info about a given film.
+  return str
+end
+
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
 
@@ -32,7 +55,15 @@ def print_movies(films)
     response_string = RestClient.get(ele)
     response_hash = JSON.parse(response_string)
     puts response_hash["title"]
+    puts response_hash["director"]
+    puts ""
   end
+end
+
+def print_director(director)
+  # some iteration magic and puts out the movies in a nice list
+
+  puts director
 end
 
 def show_character_movies(character)
@@ -40,6 +71,10 @@ def show_character_movies(character)
   print_movies(films)
 end
 
+def show_movie_director(movie)
+  director = get_movie_director_from_api(movie)
+  print_director(director)
+end
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,23 @@
-#require_relative "api_communicator.rb"
+require_relative "api_communicator.rb"
 
 def welcome
   # puts out a welcome message here!
   puts "Hellow Welcome to Star War API!"
 
+end
+def selctor
+  puts "Find director from Movie for D"
+  puts "or"
+  puts "Find character name from Movie for C"
+  input = gets.chomp
+  input.upcase!
+  if input == "D"
+    get_director_from_user
+  elsif input == "C"
+    get_character_from_user
+  else
+    puts "Error, wrong input"
+  end
 end
 
 def get_character_from_user
@@ -11,5 +25,13 @@ def get_character_from_user
   # use gets to capture the user's input. This method should return that input, downcased.
   input = gets.chomp
   input.downcase
-  return input
+  show_character_movies(input)
+end
+
+def get_director_from_user
+  puts "please enter a movie name"
+  # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
+  input.downcase
+  show_movie_director(input)
 end
